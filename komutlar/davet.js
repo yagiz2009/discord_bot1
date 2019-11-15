fc6f8b44b3ec884ec70bf6eebee140cb99a5f5ff
Glitch (busy-nose) <none>
2019-11-15 16:01:32 +0000

    🎁😀 Checkpoint ./komutlar/davet.js:7649130/1287

---
+++ komutlar/davet.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+let botid = ('644891984917561374') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${client.user.username} İletişim Bilgileri`)
+    .addField('BİZ KİMİZ?', 'Discord kullanıcıların sunucularını daha iyi hale getirmek için çabalayan bir ekibiz.')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
+    .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+    message.channel.sendEmbed(embed);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'davet',
+  description: 'bot hakkında bilgi',
+  usage: 'davetet'
+};
+   
