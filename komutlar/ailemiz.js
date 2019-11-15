2b6841079af650cf030c9aaa69dc6ef771fb4133
Glitch (busy-nose) <none>
2019-11-15 20:42:27 +0000

    🍭📮 Checkpoint ./komutlar/ailemiz.js:7649130/9

--- komutlar/ailemiz.js
+++ komutlar/ailemiz.js
@@ -8,7 +8,7 @@ exports.run = (bot, message) => {
       embed.addField(`**${guild.name}** - ÜYE SAYISI : **${guild.memberCount}**`, guild.id);
       embed.setColor('#D97634')
       embed.setTitle('Ailemiz')
-      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var ! (Gnarge)`)
+      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var !`)
       message.delete();
     }
     message.channel.send({embed: embed});
