3110f326a75ac42389e535d799db1b9d63f1bf5a
Glitch (busy-nose) <none>
2019-11-15 18:39:00 +0000

    💷📡 Checkpoint ./komutlar/mesajat.js:7649130/1300 ./komutlar/yardim-listesi.js:7649130/3663

---
+++ komutlar/yardim-listesi.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('» Komut Grupları')
+.setTimestamp()
+.addField('» Eğlence Komutları', 'f?eğlence')
+.addField('» Moderatör Komutları', 'f?moderatör')
+.addField('» Genel Komutlar', 'f?genel')
+.addField('» Ekstra Komutlar', 'f?ekstra')
+.addField('» Nsfw Komutları', 'f?nsfw')
+.setFooter('© 2018 Fredux', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'yardım-listesi',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
