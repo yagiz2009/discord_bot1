cc2c3cdc7ce0f1114e0f11857180b4c2ecc1ab32
Glitch (busy-nose) <none>
2019-11-17 14:57:32 +0000

    ❄️✨ Checkpoint ./bot.js:7649130/3200

--- bot.js
+++ bot.js
@@ -181,7 +181,7 @@ client.on('message', msg => {
 
 client.on("message", async message => {
     if (message.member.hasPermission('MANAGE_GUILD')) return;
-    let links = message.content.match[".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", "net", ".rf.gd", ".az", ".party", "discord.gg"];
+    let links = message.content.match(/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi);
     if (!links) return;
     if (message.deletable) message.delete();
     message.channel.send(`Hey ${message.author}, sunucuda link paylaşamazsın!`)
