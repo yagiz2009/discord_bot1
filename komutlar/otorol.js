cd2cdf9fc60b77f031c2faee68bf285812921eeb
Glitch (busy-nose) <none>
2019-11-18 22:22:06 +0000

    👩👬 Checkpoint ./bot.js:7649130/1332 ./komutlar/otorol.js:7649130/778 ./komutlar/otorol-kanal.js:7649130/827

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -4,13 +4,13 @@ const db = require('quick.db');
 exports.run = async (client, message, args) => {
   
   let rol = message.mentions.roles.first();
-  if (!rol) return message.reply('Bot Otorol için bir rol etiketlemelisiniz!')
+  if (!rol) return message.reply('Otorol için bir rol etiketlemelisiniz!')
   
-  db.set(`botR_${message.guild.id}`, rol.id)
+  db.set(`otoR_${message.guild.id}`, rol.id)
   
   const emb = new Discord.RichEmbed()
   .setAuthor(client.user.username, client.user.displayAvatarURL)
-  .setDescription(`Bot Otorol Rol'ü başarıyla ${rol} olarak ayarlandı!`)
+  .setDescription(`Otorol Rol'ü başarıyla ${rol} olarak ayarlandı!`)
   .setColor("RANDOM")
   .setTimestamp()
   message.channel.send(emb)
@@ -18,12 +18,12 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['bot-otorol-ayarla','bototorol-ayarla'],
+  aliases: ['otorol-ayarla','oto-rol-ayarla'],
   permLevel: 2,
   kategori: "ayar"
 }
 exports.help = {
-  name: 'bot-otorol',
-  description: 'Sunucuya özel bot otorol ayarlar.',
-  usage: '!!bot-otorol'
+  name: 'otorol',
+  description: 'Sunucuya özel otorol ayarlar.',
+  usage: '!!otorol'
 }
