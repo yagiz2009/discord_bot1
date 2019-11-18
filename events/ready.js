be5f36b30788d10bfcdd6d0400e2ab5ec6549fa0
Glitch (busy-nose) <none>
2019-11-18 08:12:38 +0000

    🏑🐸 Checkpoint ./events/ready.js:7649130/981

--- events/ready.js
+++ events/ready.js
@@ -7,16 +7,14 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "🔥Kesintiler Yaşanabilir! | !yardım",
-        "!desteksunucu✨",
-        "Tosun Bot | !yardım⭐️"  
+        "!yardım | !desteksunucu | ⭐️ Yeni Komutlar Eklendi! ",
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/emirhansaracyt" );
+        client.user.setActivity(oyun[random], "https://coderlab.glitch.com" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
@@ -24,5 +22,4 @@ var oyun = [
   client.user.setStatus("online");
   client.user.setActivity(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
