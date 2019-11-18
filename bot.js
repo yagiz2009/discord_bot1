51c1de87b094a24a795a91ac30c782e3a63e99da
Glitch (busy-nose) <none>
2019-11-18 23:49:25 +0000

    ✨🚵 Checkpoint ./bot.js:7649130/1606 ./komutlar/guvenlik.js:7649130/564

--- bot.js
+++ bot.js
@@ -427,14 +427,48 @@ client.on('guildMemberAdd', async member => {
   }
 })
 
-client.on("guildCreate", guild => {
+client.on("guildCreate", guild => { // Birisi botu sunucuya attıgında bot özel mesaj atar.
 const tesekkurler = new Discord.RichEmbed()
 .setTitle(`Tosun Bot | Bilgilendirme`)
 .setTimestamp()
 .setColor("GREEN")
-.setDescription(`Beni Sunucuna Eklediğin İçin Teşekkür Ederim \n Sana En İyi Şekilde Hizmet Edeceğim.\n Eğer Bir Sorunla Karşılaşırsan Destek Sunucuma Gel https://discord.gg/kQJQJgr\n`)
+.setDescription(`Beni Sunucuna Eklediğin İçin Teşekkür Ederim \n Sana En İyi Şekilde Hizmet Edeceğim.\n Eğer Bir Sorunla Karşılaşırsan Destek Sunucuma Gel https://discord.gg/kQJQJgr \n Komutlarımız için **!yardım** komutunu kullanınız.`)
 guild.owner.send(tesekkurler)
 
 
+});
+
+client.on('guildMemberAdd',async member => { // Güvenlik Sistemi
+  let user = client.users.get(member.id);
+  let kanal = client.channels.get(db.fetch(`guvenlik${member.guild.id}`)) 
+       const Canvas = require('canvas')
+       const canvas = Canvas.createCanvas(360,100);
+       const ctx = canvas.getContext('2d');
+  
+  const resim1 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/597433546868654106/627428441695977497/gvnlk-spheli.png')
+    const resim2 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/597433546868654106/627427731407241226/gvnlk-gvnli.png')
+    const kurulus = new Date().getTime() - user.createdAt.getTime();
+    const gün = moment(kurulus).format('dddd');  
+    var kontrol;
+      if (kurulus > 2629800000) kontrol = resim2
+    if (kurulus < 2629800000) kontrol = resim1
+
+       const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/597433546868654106/627425996454232064/gvnlk-arka.png');
+       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+   
+
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+  ctx.drawImage(kontrol,0,0,canvas.width, canvas.height)
+  ctx.beginPath();
+    ctx.lineWidth = 4;
+  ctx.fill()
+    ctx.lineWidth = 4;
+  ctx.arc(180, 46, 36, 0, 2 * Math.PI);
+    ctx.clip();
+  ctx.drawImage(avatar, 143,10, 73, 72  );
+
+   if (!kanal) return
+       const attachment = new Discord.Attachment(canvas.toBuffer(), 'güvenlik.png');
+    kanal.send(attachment)
 });
    
