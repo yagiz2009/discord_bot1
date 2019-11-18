b2db5e64bc8ea738add9879e06ca905638fd3c82
Glitch (busy-nose) <none>
2019-11-18 23:37:18 +0000

    💗⏳ Checkpoint ./bot.js:7649130/7471 ./komutlar/hg-bb.js:7649130/1432 ./ayarlar/gc.json:7649130/2

--- bot.js
+++ bot.js
@@ -427,34 +427,14 @@ client.on('guildMemberAdd', async member => {
   }
 })
 
-client.on("message", async msg => { // !seviye sistemi
-  const db = require('quick.db');
-  if (msg.channel.type === "dm") return;
-  if(msg.author.bot) return;  
-  
-  if (msg.content.length > 7) {
-    
-    db.add(`puancik_${msg.author.id + msg.guild.id}`, 1)
-};
+client.on("guildCreate", guild => {
+const tesekkurler = new Discord.RichEmbed()
+.setTitle(`Tosun Bot | Bilgilendirme`)
+.setTimestamp()
+.setColor("GREEN")
+.setDescription(`Beni Sunucuna Eklediğin İçin Teşekkür Ederim \n Sana En İyi Şekilde Hizmet Edeceğim.\n Eğer Bir Sorunla Karşılaşırsan Destek Sunucuma Gel https://discord.gg/kQJQJgr\n`)
+guild.owner.send(tesekkurler)
 
-  if (db.fetch(`puancik_${msg.author.id + msg.guild.id}`) > 150) {
-    
-    db.add(`seviye_${msg.author.id + msg.guild.id}`, 1)
-    if (msg.guild.id === "521315934825349121") return;
-    msg.channel.send(`${client.emojis.get(client.emoji.levelup)}Tebrik ederim <@${msg.author.id}>! Seviye atladın ve **${db.fetch(`seviye_${msg.author.id + msg.guild.id}`)}** seviye oldun!`)
-    db.delete(`puancik_${msg.author.id + msg.guild.id}`)};
- 
-  if (db.has(`roll_${msg.guild.id}`) === true) {
-  if (db.has(`rollss_${msg.guild.id}`) === true) {
-    
- var r = db.fetch(`roll_${msg.guild.id}`)
- var s = db.fetch(`rollss_${msg.guild.id}`)
-  
-  if (db.fetch(`seviye_${msg.author.id + msg.guild.id}`) == s) {
-    if (msg.member.roles.has(msg.guild.roles.get(r).id) === false) {
-    msg.channel.send(`<@${msg.author.id}> başarıyla **${db.fetch(`seviye_${msg.author.id + msg.guild.id}`) - 1 || 0}** seviyeyi geçtin ve **${msg.guild.roles.get(r).name}** rolünü aldın!`)
-    msg.member.addRole(msg.guild.roles.get(r).id)
-    }
-  };
-}};
+
 });
+   
