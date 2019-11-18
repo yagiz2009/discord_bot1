e4bee33afffe8043f4ec18bad4e463a1b1af9ea2
Glitch (busy-nose) <none>
2019-11-18 11:10:32 +0000

    👑💈 Checkpoint ./events/ready.js:7649130/7

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "!yardım | !desteksunucu | ⭐️ Yeni Komutlar Eklendi! ",
+        "!yardım | !desteksunucu | 🔸 Yeni Komutlar Eklendi! 🔹 ",
     ];
 
     setInterval(function() {
