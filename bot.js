2fffb6247029f697d2f99752a0f0fbf4daeb3ba1
Glitch (busy-nose) <none>
2019-11-18 14:07:11 +0000

    🛬💺 Checkpoint ./package.json:7649130/13 ./komutlar/goldyap.js:7649130/562 ./komutlar/goldtest.js:7649130/1 ./bot.js:7649130/705

--- bot.js
+++ bot.js
@@ -194,27 +194,3 @@ client.on("message", async message => {
   if(message.guild) return;
   client.channels.get('645670665784918017').send(new Discord.RichEmbed().setAuthor("Yeni Bir DM", client.user.avatarURL).setFooter(message.author.tag, message.author.avatarURL).setDescription(`**Gönderenin ID:** ${message.author.id}`).setTimestamp().addField("Mesaj", message.content).setColor("RANDOM"))
 })
-
-client.on("message", async msg => {
-const request = require('node-superfetch');
-const db = require('quick.db');
-const ms = require('parse-ms')
-let timeout = 600000
-let dakdest = await db.fetch(`goldzzz_${msg.author.id}`);
-let i = db.fetch(`gold_${msg.author.id}`)
-          if (i == 'gold') {
-    if (dakdest !== null && timeout - (Date.now() - dakdest) > 0) {
-        let time = ms(timeout - (Date.now() - dakdest));
-    } else {
-  if(msg.author.bot) return;   
-  if (msg.content.length > 1) {
-db.set(`goldzzz_${msg.author.id}`, Date.now());
-   msg.channel.send('**Bir Gold Üye Belirdi!!**')
-  }
-};
-          }
-   else if (i == undefined) {           
-          }
-          if (!i) return;
-        
-});
