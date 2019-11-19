45840ce7dc802d58bba331365755ffd53abe0ebd
Glitch (busy-nose) <none>
2019-11-19 21:26:31 +0000

    📻🧀 Checkpoint ./komutlar/dm-duyuru.js:7649130/1872

--- komutlar/dm-duyuru.js
+++ komutlar/dm-duyuru.js
@@ -2,29 +2,28 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message, args) => {
-if (message.author.id !=521315934825349121) { return; }
-  let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
-  message.delete();
-      const mesajat = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setDescription('' + mesaj + '')
-
-      client.users.forEach(u => {
-u.sendEmbed(mesajat)
-})
-message.react("‍")
-};
-
+    const mesaj = args.slice(0).join(' ');
+    if(!message.member.roles.has("644276765124198414")) return message.channel.send(`Bu komutu kullanabilmek için \`Kullanacak rol adı\` yetkisine sahip olmasınız.`);
+    if(mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setDescription(`Herkese mesaj göndere bilmem için önce mesajını yazman lazım.`).setColor('BLACK'));
+      try {
+        message.guild.members.forEach(member=> {
+        member.send(mesaj)        
+          }) 
+        message.channel.send(new Discord.RichEmbed().setDescription(`Mesaj başarıyla **` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + `** kullanıcıya gönderildi.`).setColor('RANDOM'));
+          }
+        catch(e) {
+        return console.log(`Hata`)  
+      }    
+}
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  aliases: ["dmduyuru","botduyuru"],
-  permLevel: 4
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'dmduyuru',
-  description: 'İstediğiniz şeyi bota duyurtur.',
-  usage: 'duyuru [duyurmak istediğiniz şey]'
+  description: 'DM Duyuru.',
+  usage: 'duyuru'
 };
