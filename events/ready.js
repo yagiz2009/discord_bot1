7181c2a5142173e0423597e0d6607e831baa8629
Glitch (busy-nose) <none>
2019-11-29 17:25:14 +0000

    🚵🎒 Checkpoint ./events/ready.js:7649130/19

--- events/ready.js
+++ events/ready.js
@@ -15,7 +15,7 @@ var oyun = [
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
         client.user.setActivity(oyun[random], "https://coderlab.glitch.com" );
-        }, 1 * 9999999999999);
+        }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
