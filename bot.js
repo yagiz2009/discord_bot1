91947a7ffbb30b547605dce1ae07a6c397317e08
Glitch (busy-nose) <none>
2019-11-30 18:04:12 +0000

    🛰🐝 Checkpoint ./komutlar/gold-al.js:7649130/657 ./komutlar/gold-ver.js:7649130/660 ./komutlar/gold-üye-yardım.js:7649130/1234 ./bot.js:7649130/3664 ./komutlar/gold-test.js:7649130/1968

--- bot.js
+++ bot.js
@@ -723,3 +723,50 @@ message.channel.send("⍫ Gerekli Roller 🌹")
 
 }
 });
+
+client.on("guildMemberAdd", async member => { // giriş çıkış hgbb log
+  const channel = member.guild.channels.find('name', 'giriş-çıkış');//log ismini ayarlıyacaksınız giriş-çıkış  adında kanal açın
+  if (!channel) return;
+        let username = member.user.username;
+        if (channel === undefined || channel === null) return;
+        if (channel.type === "text") {
+            const bg = await Jimp.read("https://cdn.discordapp.com/attachments/321646765180715008/520209658204651520/guildAdd.png");
+            const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./juke/"+ member.id + ".png");
+              setTimeout(function () {
+                    channel.send(new Discord.Attachment("./juke/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./juke/" + member.id + ".png");
+              }, 10000);
+        }
+    })
+client.on("guildMemberRemove", async member => {
+  const channel = member.guild.channels.find('name', 'giriş-çıkış');
+  if (!channel) return;
+        let username = member.user.username;
+        if (channel === undefined || channel === null) return;
+        if (channel.type === "text") {            
+          const bg = await Jimp.read("https://cdn.discordapp.com/attachments/321646765180715008/520209659785773056/guildRemove.png");
+ const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./juke/"+ member.id + ".png");
+              setTimeout(function () {
+                    channel.send(new Discord.Attachment("./juke/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./juke/" + member.id + ".png");
+              }, 10000);
+        }
+    }) 
