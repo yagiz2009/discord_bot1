ef3c51405247e1438ac1b002f9f61de30e5570ab
Glitch (busy-nose) <none>
2019-12-04 14:58:43 +0000

    💘🐗 Checkpoint ./komutlar/yardim-listesi.js:7649130/162 ./events/ready.js:7649130/34

--- events/ready.js
+++ events/ready.js
@@ -7,14 +7,14 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "!yardım 🔸 !desteksunucu 🔹 ",
+        "!yardım",
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://coderlab.glitch.com" );
+        client.user.setActivity(oyun[random], "https://ibots.glitch.com" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
