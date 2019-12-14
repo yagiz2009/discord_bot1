fcdf3c71abb34b45878f71cccbcfe4e9b865cd07
Glitch (busy-nose) <none>
2019-12-14 13:26:00 +0000

    🍙🏑 Checkpoint ./komutlar/yardım-kayıt.js:7649130/427 ./komutlar/yardım-yetkili.js:7649130/113

--- komutlar/yardım-kayıt.js
+++ komutlar/yardım-kayıt.js
@@ -4,11 +4,13 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
 const embed = new Discord.RichEmbed()
 .setColor('Blue')
-.setTitle('Otorol Komutları')
+.setTitle('Kayıt Komutları')
 .setTimestamp()
-.addField('!otorol','Sunucuya yeni katılan kişinin otomatik rolünü ayarlarsınız.')
-.addField('!otorol-kanal','Otorol Log Kanalını Ayarlarsınız.')
-.setFooter('iBOT Otorol Sistemi.')
+.addField('!kayıt','Mevcut Kanal Kayıtını Gösterir.')
+.addField('!kayıt-rol','Kayıt Rolünü Belirlersiniz.')
+.addField('!kayıt-log','Kayıt Log Kanalını Belirlersiniz.')
+.addField('!kayıt-kanal','Kayıt Kanalını Belirlersiniz.')
+.setFooter('iBOT Kayıt Sistemi.')
 .setTimestamp()
 .setThumbnail(client.user.avatarURL)
 message.channel.send(embed)
@@ -22,7 +24,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'otorol-sistemi',
+  name: 'kayıt-sistemi',
   description: 'Tüm komutları gösterir.',
   usage: 'yardım'
 };
