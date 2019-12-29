d479cbb28a4d5eed3148cc17dff97fe1ca3a33af
Glitch (busy-nose) <none>
2019-12-29 09:54:31 +0000

    💰🌗 Checkpoint ./komutlar/goldyap.js:7649130/1260 ./komutlar/goldçıkar.js:7649130/1199 ./komutlar/goldmarket.js:7649130/2630

---
+++ komutlar/goldmarket.js
@@ -0,0 +1,65 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+exports.run = async (client, message, args) => {
+  let istek = args[0];
+
+  let user = message.mentions.users.first() || message.member;
+  let gold = await db.fetch(`gold_${message.member.id}`);
+  let para = await db.fetch(`para_${message.author.id}`);
+
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+
+  try {
+    if (!istek) {
+      const e = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setDescription(`\n\n Ücret ile **Gold** ve **Puan** satın almak için \`zMorcy#4249\` `
+        );
+      message.channel.send(e);
+      return;
+    }
+
+    if (istek === "gold") {
+      if (gold == "kapali") {
+        const embed = new Discord.RichEmbed()
+          .setColor("BLACK")
+          .setTimestamp();
+        message.channel.send({ embed });
+      } else if (many < 3000) {
+        message.channel.send(
+          `Ne yazık ki yeterli puana sahip değilsin!`
+        );
+        return
+      } else if (many > 3000) {
+        message.channel.send(`Artık goldsun!`);
+        db.set(`many{message.member.id}`, -3000);
+        db.set(`gold_${message.member.id}`, "acik");
+        return
+      } else if ((para = 3000)) {
+        message.channel.send(`Artık goldsun!`);
+        db.set(`many{message.author.id}`, -3000);
+        db.set(`gold_${message.member.id}`, "acik");
+        return
+      }
+      return;
+    }
+    return
+  } catch (err) {
+    return;
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0,
+  kategori: "puan"
+};
+
+exports.help = {
+  name: "market",
+  description: "Eşya",
+  usage: "market"
+};
