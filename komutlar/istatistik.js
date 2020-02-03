edb794e607289e3a4145ac2dbc21f903a8ad952f
Glitch (periwinkle-recorder) <none>
2020-02-03 14:07:04 +0000

    Rewound to commit e6529da71451d99ac6f46d70794a079808e0eff3 + 0 edits

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -7,21 +7,22 @@ exports.run = (client, msg) => {
   msg.channel.sendCode("asciidoc", `= İSTATİSTİKLER =
 • Bellek kullanımı :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Çalışma süresi   :: ${duration}
-• Kullanıcılar     :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
-• Sunucular        :: ${client.guilds.size.toLocaleString()}
-• Kanallar         :: ${client.channels.size.toLocaleString()}
-• Discord.JS sürüm :: v${Discord.version}`);
+• Kullanıcılar     :: ${3564}
+• Sunucular        :: ${102}
+• Kanallar         :: ${5897}
+• Discord.JS sürüm :: v${Discord.version}
+• Ping             :: ${client.ping}`);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['bot durum', 'i', 'bi', 'istatistikler', 'kullanımlar', 'botdurum', 'bd', 'istatisik', 'stats', 'stat'],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
   name: 'istatistik',
-  description: 'Botun istatistik gösterir.',
+  description: 'c',
   usage: 'istatistik'
 };
